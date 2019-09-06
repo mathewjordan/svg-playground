9e12803d3222b812926d712f5b8fdbdc37e34b85
Mat Jordan <mat@utk.edu>
2019-09-06 13:47:12 -0400

    Refine svg slides.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import SVG from 'react-inlinesvg';
 import './css/main.css'
 
-import SVGSource from './media/svg/competency/competency_specialized.svg';
+import foundational from './media/svg/competency/competency_foundational.svg';
+import specialized from './media/svg/competency/competency_specialized.svg';
+import leadership from './media/svg/competency/competency_leadership.svg';
+import management from './media/svg/competency/competency_management.svg';
 
 function App() {
   return (
     <div className="App">
-        <SVG src={SVGSource} />
+        <SVG class="utk-svg utk-svg-animated utk-svg-animated-burst"
+             src={foundational} />
+        <SVG class="utk-svg utk-svg-animated utk-svg-animated-burst"
+             src={specialized} />
+        <SVG class="utk-svg utk-svg-animated utk-svg-animated-burst"
+             src={leadership} />
+        <SVG class="utk-svg utk-svg-animated utk-svg-animated-burst"
+             src={management} />
     </div>
   );
 }
